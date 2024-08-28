52d71d55a5731ea33fa79f0d3b7e72b9a8b08d09
Mario Reyes <ernestoreyes561@gmail.com>
2024-08-28 15:29:48 -0600

    Check landslides in polygons

--- prep-ls.R
+++ prep-ls.R
@@ -1,1 +1,8 @@
 library(tidyverse)
+library(sf)
+
+su_els <- read_sf("input/su_els_apaneca.shp")
+ls <- read_sf("input/landslides_test.shp")
+
+su_els$contains_point <- lengths(st_intersects(su_els, ls)) > 0
+
