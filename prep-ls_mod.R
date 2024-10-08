b6d72ede69d364ccd3f810d37be5eec674883523
Mario Reyes <ernestoreyes561@gmail.com>
2024-10-08 15:01:59 -0600

    checked variables preprocessing and corrected functions

--- prep-ls_mod.R
+++ prep-ls_mod.R
@@ -14,6 +14,7 @@ library(units)
 library(dismo)
 library(MIAmaxent)
 library(ENMeval)
+library(tmap)
 
 memory.limit(800000)
 
@@ -28,19 +29,60 @@ n <- 5
 ##Read data
 
 #folder name
-area <- "els"
+area <- "els_alos"
 
 #Slope units
-su <- read_sf("input/su/SU_ELS_LCC.shp") 
+su <- read_sf("input/su/SU_ELS.shp") 
 #Landslides
-ls_point <- read_sf("input/landslides/landslides_test_lcc.shp") 
+ls_point <- read_sf("input/landslides/landslides_test.shp") 
 
 #Checks if at least one landslide is inside a slope unit
 su$frane <- lengths(st_intersects(su, ls_point)) > 0
 su$frane <- as.integer(ifelse(su$frane == "TRUE", 1, 0))
 
 #Plot of su and landslides
-ggplot()+geom_sf(data = su, aes(fill=factor(frane)),linewidth=0.05)+theme_bw() 
+ggplot()+geom_sf(data = su, aes(fill=factor(frane)),linewidth=0.02)+theme_bw() 
+
+# #DEM
+# dem <- rast(paste0("input/continuous/", area, "/els_alos_30m.tif"))
+#for some reason, when the file is converted from WGS84 to UTM in QGIS the dimensions are slightly different
+# 
+# ####Variables preprocessing####
+# terra::writeRaster(dem, paste0("input/continuous/", area, "/dem.sdat"), overwrite=TRUE)
+#terra::writeRaster fails with large rasters. Converted to sdat with 30 m resolution in QGIS.
+
+#The input is a DEM in sdat format called "dem.sdat"
+
+rsaga.slope.asp.curv(in.dem = paste0("input/continuous/", area, "/dem.sdat"),
+                     out.slope = paste0("input/continuous/", area, "/slope"), unit.slope = 1,
+                     out.cprof = paste0("input/continuous/", area, "/cprof"), out.cplan = paste0("input/continuous/", area, "/cplan"),
+                     out.aspect = paste0("input/discrete/", area, "/asp.dat"), unit.aspect = 1,
+                     method = "poly2zevenbergen",
+                     env = env)
+
+# rsaga.geoprocessor(lib = "ta_morphometry",
+#                    module = "TPI Based Landform Classification",
+#                    param = list(DEM = paste0("input/continuous/", area, "/dem.sdat"),
+#                                 LANDFORMS = paste0("input/discrete/", area, "/lcl.sdat"),
+#                                 RADIUS_A_MIN = 0,
+#                                 RADIUS_A_MAX = 100,
+#                                 RADIUS_B_MIN = 0,
+#                                 RADIUS_B_MAX = 1000),
+#                                 env = env)
+
+asp <- rast(paste0("input/discrete/", area, "/asp.sdat"))
+m <- c(0, 22.5, 1,
+       22.5, 67.5, 2,
+       67.5, 112.5, 3,
+       112.5, 157.5, 4,
+       157.5, 202.5, 5,
+       202.5, 247.5, 6,
+       247.5, 292.5, 7,
+       292.5, 337.5, 8,
+       337.5, 360, 1)
+rcl <- matrix(m, ncol=3, byrow=TRUE)
+asp <- terra::classify(asp, rcl)
+writeRaster(asp, paste0("input/discrete/", area, "/asp.sdat"), overwrite=TRUE)
  
 ################################
 ## Slope unit analysis
@@ -65,6 +107,34 @@ all<-data.frame(su_model)
 #Removes NoData Values
 all=na.exclude(all)
 rownames(all) <- NULL
+
+##########################
+######Variable plots######
+##########################
+
+map <- function(var){
+  name <- "var"
+  titulo <- name
+  tm <- tm_shape(dem)+
+    tm_raster(style="cont")+
+    tm_layout(main.title = titulo, 
+              main.title.size = 1.5, 
+              main.title.position = c("left", "top"), 
+              legend.title.size = 1.2,
+              legend.text.size = 1,
+              inner.margins = c(0.2, 0.2, 0.2, 0.2))+
+    tm_scale_bar(position = c("left", "bottom"), text.size = 0.7)+
+    tm_compass(position = c("right", "top"))#+
+    # #tm_add_legend(type = "fill", 
+    #               labels = c("Depósito"),
+    #               col = NA,
+    #               border.lwd = 1.5,
+    #               border.col = "red")
+  #tmap_save(tm, paste0("../imagenes/", zona_comp, name,".png"), asp=get_asp_ratio(tm))
+  return(tm)
+}
+
+
 
 ################################
 #Calibration and validation data
@@ -108,25 +178,25 @@ return(list(all_cal14, all_val14))
 
 }
 
-random <- cal_val(all)
+calval_random <- cal_val(all)
 
-##########################
-#MARS modeling
-##########################
+#####################
+####MARS modeling####
+#####################
 mars_model <- function(cal, val){
 
 mars_all<-NULL
-all_predict<-matrix(nrow=nrow(all_val14[[i]]), ncol = n)
+all_predict<-matrix(nrow=nrow(val[[1]]), ncol = n)
 AUC_all<-matrix(nrow=1, ncol=n)
 
 ####Regression analysis
 ####Cambiar esta linea si se cambian variables####
 
 for(i  in 1:n){
-  mars_all[[i]]<- earth (reformulate(names(dplyr::select(all, -c(DN, frane, geometry))), "frane"), data = all_cal14[[i]], trace = 1, degree=1,  glm=list(family=binomial), Scale.y=FALSE)}
+  mars_all[[i]]<- earth (reformulate(names(dplyr::select(all, -c(DN, frane, geometry))), "frane"), data = cal[[i]], trace = 1, degree=1,  glm=list(family=binomial), Scale.y=FALSE)}
 
 for(i  in 1:n){
-  all_predict[,i]<-predict(mars_all[[i]], all_val14[[i]], type=c("response"))
+  all_predict[,i]<-predict(mars_all[[i]], val[[i]], type=c("response"))
 }
 
 summary_earth <- lapply(1:n, function(x) capture.output(summary(mars_all[[x]]), file = "salida/summary_earth.csv", append = TRUE))
@@ -135,11 +205,11 @@ return(all_predict)
 
 }
 
-mars_random <- mars_model(random[[1]], random[[2]])
+mars_random <- mars_model(calval_random[[1]], calval_random[[2]])
 
-#################################
-####ROC models function####
-#################################
+############################
+####ROC models function#####
+############################
 
 roc_model <- function(vec, all_predict){
   
@@ -171,12 +241,15 @@ roc_model <- function(vec, all_predict){
 }
 
 #Validation data
-roc_random <- roc_model(random[[2]], mars_random)
+roc_random <- roc_model(calval_random[[2]], mars_random)
 
 #################################
 ####Mean ROC models function#####
 #################################
-roc_model <- function(vec){
+
+#Input is vec(validation or calibration data) and all_predict (output of mars_model)
+
+roc_model_m <- function(vec, all_predict){
 casiallc<-matrix(nrow = nrow(vec[[1]]), ncol=n)
 
 for(i  in 1:n){
@@ -196,14 +269,26 @@ return(list(roc_all_m, youdenall_m, auc_all_m))
 
 }
 
+roc_m_random <- roc_model(calval_random[[2]], mars_random)
+
 #################################
 ####Plot ROC models function#####
 #################################
 
+#Inputs are roc_all (output of roc_model) and roc_all_m (output of roc_model_m)  
+
+plot_roc <- function(roc_all, roc_all_m){
+
 mean_roc <- data.frame(x=1-roc_all_m$specificities, y=roc_all_m$sensitivities)
 
-ggroc(roc_all, legacy.axes = T)+geom_line(color="gray")+
+p<- ggroc(roc_all, legacy.axes = T)+geom_line(color="gray")+
   geom_line(data = mean_roc, aes(x, y), color="red", inherit.aes = FALSE)+theme_bw()+theme(legend.position="none")
+
+return(p)
+
+}
+
+plot_roc(roc_random, roc_m_random)
 
 ###################
 ####all_map####
